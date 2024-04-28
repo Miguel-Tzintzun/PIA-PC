75b4228951fed9dd1957268659ded6c6a2f5460e
Miguel Angel Aguila Tzintzun <84878958+Miguel-Tzintzun@users.noreply.github.com>
2024-04-28 10:58:53 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,36 +1,39 @@
-import web_scraping
-import port_scanning
-import encryption
-import hashing
-import metadata
+import menuwebs
+#imports
 
 def main():
     while True:
         print("\nMenú:")
         print("1. Web Scraping")
-        print("2. Escaneo de Puertos")
-        print("3. Cifrado y Descifrado de Mensajes")
-        print("4. Obtención de Hash")
-        print("5. Revisión de Encabezados y Metadatos")
-        print("6. Salir")
+        print("2. ¿La URL es segura?")
+        print("3. Enviar PDF a un sms")
+        print("4. ")
+        print("5. ")
+        print("6. ")
+        print("7. Salir")
 
         opcion = input("\nSelecciona una opción: ")
 
-        if opcion == "1":
-            web_scraping.web_scraping()
-        elif opcion == "2":
-            port_scanning.port_scanning()
-        elif opcion == "3":
-            encryption.encryption()
-        elif opcion == "4":
-            hashing.hashing()
-        elif opcion == "5":
-            metadata.metadata()
-        elif opcion == "6":
-            print("Saliendo del programa...")
-            break
-        else:
-            print("Opción no válida. Inténtalo de nuevo.")
+        try:
+            if opcion == "1":
+                menuwebs.menuwebs()
+            elif opcion == "2":
+                ()
+            elif opcion == "3":
+                ()
+            elif opcion == "4":
+                ()
+            elif opcion == "5":
+                ()
+            elif opcion == "6":
+                ()
+            elif opcion == "7":
+                print("Saliendo del programa...")
+                break
+            else:
+                print("Opción no válida. Inténtalo de nuevo.")
+        except Exception as e:
+            print(f"Er1ror: {e}")
 
 if __name__ == "__main__":
     main()
