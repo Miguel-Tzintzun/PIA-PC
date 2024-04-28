afa9f580bbe411a31efda0092f2268e2af8ae06f
Miguel Angel Aguila Tzintzun <84878958+Miguel-Tzintzun@users.noreply.github.com>
2024-04-28 11:00:40 -0600

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,29 +1,60 @@
-import requests
-from bs4 import BeautifulSoup
+#! python3
 
-def web_scraping():
-    #URL de la pagina a scrapear
-    url = 'https://www.bbc.com/news'
+import requests, bs4, os
 
-    #Realizar la solicitud GET a la página
-    response = requests.get(url)
-
-    #Verificar si la solicitud fue exitosa (código de estado 200)
-    if response.status_code == 200:
-        #Parsear el contenido HTML
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        #Encontrar todas las etiquetas 'a' que contienen los enlaces a las noticias
-        links = soup.find_all('a', class_='gs-c-promo-heading')
-
-        #Imprimir el título y el enlace de cada noticia
-        for link in links:
-            title = link.text.strip() # Obtener el texto del titulo
-            href = link['href']     #Obtener el enlace (atributo href)
-            print(f"Título: {title}")
-            print(f"Enlace: {href}")
-            print("--------")
+def verificador_nav(url):
+    if url.startswith("http://") or url.startswith("https://"):
+        return url
     else:
-        print("Error al cargar la página: ", response.status_code)
-    pass
+        return "http://" + url
+    
+def conexion_request(url_nueva):
+    os.makedirs('Imagenes', exist_ok=True)
+    os.makedirs('PDFs', exist_ok=True)
+    print('Descargando pagina %s...' % url_nueva)
+    req = requests.get(url_nueva)
+    sopa = bs4.BeautifulSoup(req.text, "html.parser")
+    
+    imagenes = sopa.find_all('img')
+    if imagenes == []:
+        print('No se encontró.')
+    else:
+        for img in imagenes:
+            imagenesUrl = img.get('src')
+            if imagenesUrl.startswith("http"):
+                print('Descargando %s...' % (imagenesUrl))
+                response = requests.get(imagenesUrl)
+                if response.status_code == 200:
+                    imageFile = open(os.path.join('Imagenes', os.path.basename(imagenesUrl)),'wb')
+                    for chunk in response.iter_content(100000):
+                        imageFile.write(chunk)
+                    imageFile.close()
+            else:
+                print('Descargando %s...' % (imagenesUrl))
+                imagenesUrl = url_nueva + imagenesUrl
+                response = requests.get(imagenesUrl)
+                if response.status_code == 200:
+                    imageFile = open(os.path.join('Imagenes', os.path.basename(imagenesUrl)),'wb')
+                    for chunk in response.iter_content(100000):
+                        imageFile.write(chunk)
+                    imageFile.close()
+    
+    hipervinculos = sopa.find_all('a')
+    with open("hipervinculos.txt", "w") as f:
+        for hi in hipervinculos:
+            link = hi.get("href")
+            if link:
+                f.write(link + "\n")
+        print("Se guardaron correctamente los hipervinculos")
 
+    
+    PDFs = sopa.find_all('a')
+    for p in PDFs:
+        if ('.pdf' in p.get('href', [])):
+            response = requests.get(p.get('href'))
+            nombre = p.get('href')
+            pdf = open(os.path.join('PDFs', os.path.basename(nombre)), 'wb')
+            pdf.write(response.content)
+            pdf.close()
+            print("archivo ", nombre , " fue descargado")
+    print("Se descargaron todos los PDFs")
