cc3f4b70b0a470aa92e4bbcae8b39deb6639244f
Miguel Angel Aguila Tzintzun <84878958+Miguel-Tzintzun@users.noreply.github.com>
2024-05-05 20:06:43 -0600

    Update menuwebs.py

--- menuwebs.py
+++ menuwebs.py
@@ -1,12 +1,13 @@
+#Importamos el modulo web_scraping.py
 import web_scraping
 
 def menuwebs():
-    url = input("Ingrese la URL: ")
-    url_nueva = web_scraping.verificador_nav(url)
-    html = web_scraping.conexion_request(url_nueva)
+    url = input("Ingrese la URL: ") #Solicitamos al usuario que ingrese una URL
+    url_nueva = web_scraping.verificador_navegacion(url) #llamamos a la función para verificar y corregir la URL ingresada
+    html = web_scraping.conexion_request(url_nueva) #Llamada a la función para estabecler una conexión con la pagina web
     
-    print(html)
+    print(html) #Se imprime el contenido HTML de la página web
 
-
+#Esta parte asegura que la función 'menuwebs() se ejecute solo si el script se esta ejecutando directamente'
 if __name__=='__main__':
     menuwebs()
