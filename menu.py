524bbd5819f26002e6c6ef7b5a7bb86966463687
Miguel Angel Aguila Tzintzun <84878958+Miguel-Tzintzun@users.noreply.github.com>
2024-05-05 20:06:19 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,24 +1,36 @@
-import menuwebs
+"""
+Importamos los modulos 'menwebs' y  'menuverify', estos son modulos
+personalizados que contienen funciones para realizar oprraciones específicas
+"""
+import menuwebs 
+import menuverify
 #imports
 
+
+"""Definimos la función main() que será la función principal del programa,
+el bucle while True garantiza que el menú se mantega mostrande indefinidamente hasta que el
+usuario decida salir"""
 def main():
     while True:
+#Imprime el menú en la consola con varias opciones que el usuario elija
         print("\nMenú:")
         print("1. Web Scraping")
-        print("2. ¿La URL es segura?")
+        print("2. Verificador de URLs")
         print("3. ")
         print("4. ")
         print("5. ")
         print("6. ")
         print("7. Salir")
 
+#Solicitamos al usuario que ingrese la opcion
         opcion = input("\nSelecciona una opción: ")
 
         try:
             if opcion == "1":
                 menuwebs.menuwebs()
             elif opcion == "2":
-                ()
+                base_url = input("Enter the URL of the webpage: ")
+                menuverify.menuverify(base_url)
             elif opcion == "3":
                 ()
             elif opcion == "4":
@@ -32,8 +44,12 @@ def main():
                 break
             else:
                 print("Opción no válida. Inténtalo de nuevo.")
+#Maneja cualquier excepción que pueda ocurrir durante a ejecución del codigo e imprime un mensaje de error
         except Exception as e:
             print(f"Er1ror: {e}")
 
+"""Esta linea verifica si el scriptse está ejecunado directamente y,
+ si es así, llama a la función main() para iniciar el programa"""
 if __name__ == "__main__":
     main()
+    
