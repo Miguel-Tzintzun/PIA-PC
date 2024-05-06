e10b407533b85bbc3869b7f599ae779689c074e7
Miguel Angel Aguila Tzintzun <84878958+Miguel-Tzintzun@users.noreply.github.com>
2024-05-05 20:07:14 -0600

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,13 +1,29 @@
 #! python3
+#Indicamos que el script debe ser ejecutado utilizando python 3.
 
-import requests, bs4, os
 
-def verificador_nav(url):
+"""Se importan los módulos necesarios para realizar operaciones relacionadas con la web 
+(requests para hacer solicitudes HTTP, bs4 para el análisis HTML
+ y os para operaciones del sistema operativo)."""
+import requests
+import bs4
+import os
+
+
+"""Esta función toma una URL como entrada y verifica 
+si comienza con "http://" o "https://". Si no lo hace, 
+agrega "http://" al principio y devuelve la URL modificada."""
+def verificador_navegacion(url):
     if url.startswith("http://") or url.startswith("https://"):
         return url
     else:
         return "http://" + url
     
+
+"""La siguiente función crea dos directorios, uno para almacenar imágenes 
+(Imagenes) y otro para almacenar PDFs (PDFs). 
+Luego realiza una solicitud HTTP a la URL proporcionada (url_nueva) y crea 
+un objeto BeautifulSoup para analizar el contenido HTML de la página."""
 def conexion_request(url_nueva):
     os.makedirs('Imagenes', exist_ok=True)
     os.makedirs('PDFs', exist_ok=True)
@@ -15,7 +31,7 @@ def conexion_request(url_nueva):
     req = requests.get(url_nueva)
     sopa = bs4.BeautifulSoup(req.text, "html.parser")
     
-    imagenes = sopa.find_all('img')
+    imagenes = sopa.find_all('img') #Busca todas las etiquetas <img> en la página web y obtiene la URL de la fuente de cada imagen
     if imagenes == []:
         print('No se encontró.')
     else:
@@ -39,6 +55,7 @@ def conexion_request(url_nueva):
                         imageFile.write(chunk)
                     imageFile.close()
     
+    """Encuentra todos los hipervínculos (<a> tags) en la página y lo guardaremos en un archivo de texto llamado hipervinculos.txt"""
     hipervinculos = sopa.find_all('a')
     with open("hipervinculos.txt", "w") as f:
         for hi in hipervinculos:
@@ -47,7 +64,7 @@ def conexion_request(url_nueva):
                 f.write(link + "\n")
         print("Se guardaron correctamente los hipervinculos")
 
-    
+    """Encuentra todos los hipervínculos en la página y verifica si apuntan a archivos PDF. Si lo hacen, descarga los PDFs y los guarda en el directorio PDFs."""
     PDFs = sopa.find_all('a')
     for p in PDFs:
         if ('.pdf' in p.get('href', [])):
