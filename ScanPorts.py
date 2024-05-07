c3a6a112d24229b20f1ead98e18582c5198a8689
Naizard <163219749+Naizard@users.noreply.github.com>
2024-05-06 18:01:28 -0600

    Add files via upload

---
+++ ScanPorts.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+
+#Verificar puerto 25
+def check_ports(ip_address):
+    ports_to_check = [21, 22, 25, 80, 443]  
+    results = []
+    print(f"Verificando puertos abiertos en {ip_address}...")
+    for port in ports_to_check:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            result = s.connect_ex((ip_address, port))
+            if result == 0:
+                status = "Abierto"
+            else:
+                status = "Cerrado"
+            results.append((port, status))
+            s.close()
+        except Exception as e:
+            results.append((port, "Error"))
+    return results
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python ScanPorts.py <dirección_IP>")
+        sys.exit(1)
+    ip_address = sys.argv[1]
+    results = check_ports(ip_address)
+    for port, status in results:
+        print(f"Puerto {port}: {status}")
