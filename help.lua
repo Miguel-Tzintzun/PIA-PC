6df5aa9f313135d0f28039060aaf78421914ec5f
Miguel Angel Aguila Tzintzun <84878958+Miguel-Tzintzun@users.noreply.github.com>
2024-05-12 20:10:18 -0600

    Add files via upload
    ACTUALIZADO PIA PC

---
+++ help.lua
@@ -0,0 +1,14 @@
+usage: arg.py [-h] [-w] [-u URL] [-p] [-m] [-hp] [-a]
+
+Este es un programa con varias opciones.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w, --webscraping     Realizar Web Scraping
+  -u URL, --urlverify URL
+                        Verificar una URL
+  -p, --portscan        Escanear Puertos
+  -m, --metadata        Mostrar Metadatos
+  -hp, --haveibeenpwned
+                        Consultar si una cuenta ha sido comprometida
+  -a, --automate        Automatización de Análisis de Malware
