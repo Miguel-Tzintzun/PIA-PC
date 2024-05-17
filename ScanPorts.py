b69a160dfee29df85bc445bb92110054e2c074e0
Naizard <163219749+Naizard@users.noreply.github.com>
2024-05-17 16:00:37 -0600

    Update ScanPorts.py

--- ScanPorts.py
+++ ScanPorts.py
@@ -1,27 +1,29 @@
 import socket
 import sys
+import logging
 
-#Verificar puerto 25
+logging.basicConfig(filename='error_puerto.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def check_ports(ip_address):
-    ports_to_check = [21, 22, 25, 80, 443]  
+    port_check = [-1, 21, 22, 25, 80, 443]  
     results = []
-    print(f"Verificando puertos abiertos en {ip_address}...")
-    for port in ports_to_check:
+    logging.info(f"Verificando puertos abiertos en {ip_address}.")
+    for port in port_check:
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(1)
-            result = s.connect_ex((ip_address, port))
-            if result == 0:
-                status = "Abierto"
-            else:
-                status = "Cerrado"
-            results.append((port, status))
-            s.close()
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(1)
+                result = s.connect_ex((ip_address, port))
+                status = "Abierto" if result == 0 else "Cerrado"
+                results.append((port, status))
+                logging.info(f"Puerto {port}: {status}")
         except Exception as e:
             results.append((port, "Error"))
+            logging.error(f"Error al verificar el puerto {port}: {e}")
     return results
-    
+
 ip_address = sys.argv[1]
+print(f"Dirección IP: {ip_address}")
+print("Puertos:")
 results = check_ports(ip_address)
 for port, status in results:
     print(f"Puerto {port}: {status}")
